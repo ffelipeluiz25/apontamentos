7997655bc1ae35741ffb8cd3e56ca06512c1dba7
Luiz Felipe <ffelipeluiz25@gmail.com>
2023-05-16 21:55:16 -0300

    ajuste para gerador pdf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ApontamentosAngularComponent } from './components-angular/apontamentos-angular.component';
 import { GeradorPdfComponent } from './components-angular/gerador-pdf/gerador-pdf.component';
 
 const routes: Routes = [
-  { path: 'apontamentos', component: ApontamentosAngularComponent },
   { path: 'gerador-pdf', component: GeradorPdfComponent },
   { path: '', component: GeradorPdfComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ApontamentosAngularComponent } from './components-angular/apontamentos-angular.component';
-import { DetalhesAngularComponent } from './components-angular/detalhes/detalhes.component';
 import { ApiService } from './servicos/api-servico';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Util } from './utils/utils';
 import { GeradorPdfComponent } from './components-angular/gerador-pdf/gerador-pdf.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ApontamentosAngularComponent,
-    GeradorPdfComponent,
-    DetalhesAngularComponent
+    GeradorPdfComponent
   ],
   imports: [
     BrowserModule,
